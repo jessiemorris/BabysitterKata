4d3a1c3fd9e5fcd27c082fffeac7e9fff7ed51da
Jessie Morris <jessiecodes@gmail.com>
2016-05-05 18:58:13 -0400

    Added QUnit modules to group together test cases to making reviewing them easier to manage.  Re-organized the order of the tests in what feels like a more logical order for now.

--- tests.js
+++ tests.js
@@ -1,55 +1,9 @@
+QUnit.module("Harness Integration Tests");
 QUnit.test( "TestHarnessIntegrationSuccessful", function(assert) {
 	assert.equal(1, 1, "Text harness properly configured!" );
 });
 
-QUnit.test( "BabysitterWorksOneHourBeforeBedtimeAndReceives12Dollars", function(assert) {
-	var startTime = 17;  //5:00 PM
-	var endTime = 18;    //6:00 PM
-	var bedTime = 21;    //9:00 PM
-
-	assert.equal(calculatePayment(startTime, endTime, bedTime), 12);
-});
-
-QUnit.test( "BabysitterWorksTwoHoursBeforeBedtimeAndReceives24Dollars", function(assert) {
-	var startTime = 17;  //5:00 PM
-	var endTime = 19;    //7:00 PM
-	var bedTime = 21;    //9:00 PM
-
-	assert.equal(calculatePayment(startTime, endTime, bedTime), 24);
-});
-
-QUnit.test( "BabysitterWorksOneHourAfterBedtimeAndBeforeMidnightAndReceives8Dollars", function(assert) {
-	var startTime = 21;  //9:00 PM
-	var endTime = 22;    //10:00 PM
-	var bedTime = 21;    //9:00 PM
-
-	assert.equal(calculatePayment(startTime, endTime, bedTime), 8);
-});
-
-QUnit.test( "BabysitterWorksTwoHoursAfterBedtimeAndBeforeMidnightAndReceives16Dollars", function(assert) {
-	var startTime = 21;  //9:00 PM
-	var endTime = 23;    //11:00 PM
-	var bedTime = 21;    //9:00 PM
-
-	assert.equal(calculatePayment(startTime, endTime, bedTime), 16);
-});
-
-QUnit.test( "BabysitterWorksOneHourAfterMidnightAndReceives16Dollars", function(assert) {
-	var startTime = 0;   //12:00 AM
-	var endTime = 1;     //1:00 AM
-	var bedTime = 21;    //9:00 PM
-
-	assert.equal(calculatePayment(startTime, endTime, bedTime), 16);
-});
-
-QUnit.test( "BabysitterWorksTwoHoursAfterMidnightAndReceives32Dollars", function(assert) {
-	var startTime = 0;   //12:00 AM
-	var endTime = 2;     //2:00 AM
-	var bedTime = 21;    //9:00 PM
-
-	assert.equal(calculatePayment(startTime, endTime, bedTime), 32);
-});
-
+QUnit.module("Zero Cases");
 QUnit.test( "BabysitterWorksZeroHoursAndReceives0Dollars_BeforeBedtime", function(assert) {
 	var startTime = 17;   //5:00 PM
 	var endTime = 17;     //5:00 PM
@@ -73,3 +27,57 @@ QUnit.test( "BabysitterWorksZeroHoursAndReceives0Dollars_AfterMidnight", functio
 
 	assert.equal(calculatePayment(startTime, endTime, bedTime), 0);
 });
+
+QUnit.module("Before Bedtime Tests");
+QUnit.test( "BabysitterWorksOneHourBeforeBedtimeAndReceives12Dollars", function(assert) {
+	var startTime = 17;  //5:00 PM
+	var endTime = 18;    //6:00 PM
+	var bedTime = 21;    //9:00 PM
+
+	assert.equal(calculatePayment(startTime, endTime, bedTime), 12);
+});
+
+QUnit.test( "BabysitterWorksTwoHoursBeforeBedtimeAndReceives24Dollars", function(assert) {
+	var startTime = 17;  //5:00 PM
+	var endTime = 19;    //7:00 PM
+	var bedTime = 21;    //9:00 PM
+
+	assert.equal(calculatePayment(startTime, endTime, bedTime), 24);
+});
+
+QUnit.module("Bedtime->Midnight Tests");
+QUnit.test( "BabysitterWorksOneHourAfterBedtimeAndBeforeMidnightAndReceives8Dollars", function(assert) {
+	var startTime = 21;  //9:00 PM
+	var endTime = 22;    //10:00 PM
+	var bedTime = 21;    //9:00 PM
+
+	assert.equal(calculatePayment(startTime, endTime, bedTime), 8);
+});
+
+QUnit.test( "BabysitterWorksTwoHoursAfterBedtimeAndBeforeMidnightAndReceives16Dollars", function(assert) {
+	var startTime = 21;  //9:00 PM
+	var endTime = 23;    //11:00 PM
+	var bedTime = 21;    //9:00 PM
+
+	assert.equal(calculatePayment(startTime, endTime, bedTime), 16);
+});
+
+QUnit.module("After Midnight Tests");
+QUnit.test( "BabysitterWorksOneHourAfterMidnightAndReceives16Dollars", function(assert) {
+	var startTime = 0;   //12:00 AM
+	var endTime = 1;     //1:00 AM
+	var bedTime = 21;    //9:00 PM
+
+	assert.equal(calculatePayment(startTime, endTime, bedTime), 16);
+});
+
+QUnit.test( "BabysitterWorksTwoHoursAfterMidnightAndReceives32Dollars", function(assert) {
+	var startTime = 0;   //12:00 AM
+	var endTime = 2;     //2:00 AM
+	var bedTime = 21;    //9:00 PM
+
+	assert.equal(calculatePayment(startTime, endTime, bedTime), 32);
+});
+
+
+
